6c27d215e18e116fcd7a897ccee11fa7712f4183
Seb-Nutt <nuttsebastian@gmail.com>
2026-03-02 15:26:01 -0600

    added descriptions, condensed constants into objects, finished the velocity sytem and added a small bounce

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -3,7 +3,7 @@
 // Date
 //
 // Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// I created a velocity and gravity system
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -12,10 +12,12 @@ function setup() {
     cloudsY.push(random(0,height*2/3));
     cloudsWidth.push(random(200,400));
     cloudsHeight.push(random(25,100));
-    postHeight = height/3;
-    // ball.x = width/4;
-    // ball.y = 7*height/8;
   }
+  postHeight = height/3;
+  ball.x = width/4;
+  ball.y = 7*height/8;
+  ball.xVelocity = 0;
+  ball.yVelocity = 0;
 }
 
 //Define variables
@@ -36,7 +38,12 @@ let ball = {
   verticalDirection: 1,
   yShift: 0
 };
-
+let rim = {
+  X1: 0,
+  Y1: 0,
+  LENGTH: 100,
+  RIM_HEIGHT: 50
+};
 let lastSwitch = 0;
 let score = 0;
 
@@ -90,17 +97,37 @@ function drawHoop(){
   //draw the backboard
   strokeWeight(1);
   fill('white');
+
+  // define the backboard constants
+
+  let backboard = {
+
+  };
+  
   quad(13*width/16, postHeight + height/8, 15*width/16, postHeight + height/5, 15*width/16, postHeight + height/32, 13*width/16, postHeight - height/32);
+
+  // define the backboardsquare constants
+  let backboardSquare = {
+    LEFT_X: 27*width/32,
+    RIGHT_X: 29*width/32,
+    TOP_LEFT_Y: postHeight + 37*height/256,
+    BOTTOM_LEFT_Y: postHeight + height/16,
+    TOP_RIGHT_Y: postHeight + height/10,
+    BOTTOM_RIGHT_Y: postHeight + 2*height/11
+  };
   
   //draw the square on the backboard
-  quad(27*width/32, postHeight + 37*height/256, 27*width/32, postHeight + height/16, 29*width/32, postHeight + height/10, 29*width/32, postHeight + 2*height/11);
+  quad(backboardSquare.LEFT_X, backboardSquare.TOP_LEFT_Y, backboardSquare.LEFT_X, backboardSquare.BOTTOM_LEFT_Y, backboardSquare.RIGHT_X, backboardSquare.TOP_RIGHT_Y, backboardSquare.RIGHT_X, backboardSquare.BOTTOM_RIGHT_Y);
   
-  // draw rim
-  // layering is based on the order drawn
+  // draw the backround rim
+  rim.X1 = 27*width/32;
+  rim.Y1 = postHeight+2*height/11;
+  rim.RIM_HEIGHT = height/16;
   stroke('red');
   strokeWeight(10);
   noFill();
-  ellipse(27*width/32,postHeight+2*height/11, 100, 50);
+  ellipse(rim.X1,rim.Y1, rim.LENGTH, rim.RIM_HEIGHT);
+
 }
 
 function drawBall(){
@@ -114,14 +141,26 @@ function drawBall(){
   line(ball.x - 7*ball.radius/8, ball.y - ball.radius/2, ball.x + 7*ball.radius/8, ball.y - ball.radius/2);
 }
 
+function drawForegroundRim(){
+  stroke('red');
+  strokeWeight(10);
+  noFill();
+  bezier(27*width/32,postHeight+2*height/11,10,10,100,50);
+}
+
 function mouseOnBall(){
   return dist(mouseX,mouseY,ball.x,ball.y) < 3*ball.radius;
 }
 
 function moveBall(){
+
+  // Let the player drag the ball
   if (mouseOnBall() && mouseIsPressed){
     ball.x = mouseX;
     ball.y = mouseY;
+
+    // Reset the verticle direction to upward
+    ball.verticalDirection = 1;
   }
   // if (ball.y < height-height/8 && !mouseIsPressed){
   //   ball.y += ball.yVelocity;
@@ -134,31 +173,36 @@ function moveBall(){
     
     ball.xVelocity = calculateVelocity()[0];
     ball.yVelocity = calculateVelocity()[1];
+
+    if (ball.y < height-height/8 && (!mouseIsPressed || !mouseOnBall())){
+      ball.yShift += 1;
+    }
+    // else{
+    //   ball.yShift = 0;
+    //   ball.yVelocity = 0;
+    // }
+
+
+    // Move the ball if its isnt being held
     if (!mouseIsPressed || !mouseOnBall()){
       ball.x += ball.xVelocity/10;
       ball.y += ball.yVelocity/10 * ball.verticalDirection;
     }
 
     // bounce ball
-    // if (ball.y > 7*height/8){
-    //   ball.y = 7*height/8;
-    //   ball.yVelocity*= -1;
-    // }
+    if (ball.y > 7*height/8){
+      ball.y = 7*height/8;
+      ball.verticalDirection*= -1;
+      ball.yShift = 0;
+    }
     ball.xVelocity--;
     ball.y += ball.yShift/10;
-  }
-  
-  if (ball.y < height-height/8 && (!mouseIsPressed || !mouseOnBall())){
-    ball.yShift += 1;
-  }
-  else{
-    ball.yShift = 0;
-    ball.yVelocity = 0;
   }
 }
 
 function calculateVelocity(){
-// make a y velocity one
+
+  // update the units per second
   if (millis() > lastSwitch+100 && ballIn()){
     ball.changeX = ball.x - ball.lastXPosition;
     ball.lastXPosition = ball.x;
@@ -167,7 +211,7 @@ function calculateVelocity(){
     lastSwitch = millis();
   }
 
-
+  //return the change in X and the change in Y
 
   return [ball.changeX,ball.changeY];
 }
