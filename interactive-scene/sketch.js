d527aa04634c283940fa04e0dd62a3834816a6db
Seb-Nutt <nuttsebastian@gmail.com>
2026-03-02 20:47:29 -0600

    I added descriptions to my code, condensted information, finished the rim system, finished the scoring system, updated the text displays score

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -3,21 +3,20 @@
 // Date
 //
 // Extra for Experts:
-// I created a velocity and gravity system
+// I created a velocity and gravity system to apply to the ball and I used bezier curves to create a layered rim
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
+  //create two clouds at random positions
   for (let i = 0; i < 2; i++){
     cloudsX.push(random(0,width));
     cloudsY.push(random(0,height*2/3));
     cloudsWidth.push(random(200,400));
     cloudsHeight.push(random(25,100));
   }
+  //set a default fot the height of the post
   postHeight = height/3;
-  ball.x = width/4;
-  ball.y = 7*height/8;
-  ball.xVelocity = 0;
-  ball.yVelocity = 0;
 }
 
 //Define variables
@@ -27,10 +26,10 @@ let cloudsWidth = [];
 let cloudsHeight = [];
 let ball = {
   xVelocity: 0,
-  x: 0,
+  x: 50,
   yVelocity: 0,
-  y: 0,
-  radius: 25,
+  y: 50,
+  RADIUS: 25,
   changeX: 0,
   changeY: 0,
   lastXPosition: 0,
@@ -40,12 +39,17 @@ let ball = {
 };
 let rim = {
   X1: 0,
-  Y1: 0,
+  y: 0,
   LENGTH: 100,
-  RIM_HEIGHT: 50
+  RIM_HEIGHT: 50,
+  Y2: 0,
+  RADIUS: 5*ball.RADIUS/2
 };
 let lastSwitch = 0;
 let score = 0;
+let scored = false;
+
+
 
 function draw() {
   background('navy');
@@ -53,6 +57,7 @@ function draw() {
   drawNonInteractives();
   drawHoop();
   drawBall();
+  drawForegroundRim();
   moveBall();
   detectScore();
   displayScore();
@@ -82,6 +87,7 @@ function drawNonInteractives(){
 }
 
 function drawHoop(){
+
   // Ajust the height of the post with the up and down arrows
   if (keyIsDown(38)){
     postHeight--;
@@ -101,55 +107,64 @@ function drawHoop(){
   // define the backboard constants
 
   let backboard = {
-
+    LEFT_X: 13*width/16,
+    RIGHT_X: 15*width/16,
+    TOP_LEFT_Y: postHeight - height/32,
+    BOTTOM_LEFT_Y: postHeight + height/32,
+    TOP_RIGHT_Y: postHeight + height/5,
+    BOTTOM_RIGHT_Y: postHeight + height/8
   };
   
-  quad(13*width/16, postHeight + height/8, 15*width/16, postHeight + height/5, 15*width/16, postHeight + height/32, 13*width/16, postHeight - height/32);
+  quad(backboard.LEFT_X, backboard.TOP_LEFT_Y, backboard.RIGHT_X, backboard.BOTTOM_LEFT_Y, backboard.RIGHT_X, backboard.TOP_RIGHT_Y, backboard.LEFT_X, backboard.BOTTOM_RIGHT_Y);
 
   // define the backboardsquare constants
   let backboardSquare = {
     LEFT_X: 27*width/32,
     RIGHT_X: 29*width/32,
-    TOP_LEFT_Y: postHeight + 37*height/256,
-    BOTTOM_LEFT_Y: postHeight + height/16,
+    TOP_LEFT_Y: postHeight + height/16,
+    BOTTOM_LEFT_Y: postHeight + 37*height/256,
     TOP_RIGHT_Y: postHeight + height/10,
     BOTTOM_RIGHT_Y: postHeight + 2*height/11
   };
   
   //draw the square on the backboard
-  quad(backboardSquare.LEFT_X, backboardSquare.TOP_LEFT_Y, backboardSquare.LEFT_X, backboardSquare.BOTTOM_LEFT_Y, backboardSquare.RIGHT_X, backboardSquare.TOP_RIGHT_Y, backboardSquare.RIGHT_X, backboardSquare.BOTTOM_RIGHT_Y);
+  quad(backboardSquare.LEFT_X, backboardSquare.BOTTOM_LEFT_Y, backboardSquare.LEFT_X, backboardSquare.TOP_LEFT_Y, backboardSquare.RIGHT_X, backboardSquare.TOP_RIGHT_Y, backboardSquare.RIGHT_X, backboardSquare.BOTTOM_RIGHT_Y);
   
-  // draw the backround rim
+  // draw the backround rim to go behind the ball
   rim.X1 = 27*width/32;
-  rim.Y1 = postHeight+2*height/11;
+  rim.y = postHeight+2*height/11;
   rim.RIM_HEIGHT = height/16;
   stroke('red');
   strokeWeight(10);
   noFill();
-  ellipse(rim.X1,rim.Y1, rim.LENGTH, rim.RIM_HEIGHT);
-
+  bezier(rim.X1 - rim.RADIUS, rim.y, rim.X1 - rim.RADIUS/2, rim.y - rim.RIM_HEIGHT/2, rim.X1 + rim.RADIUS/2, rim.y - rim.RIM_HEIGHT/2, rim.X1 + rim.RADIUS, rim.y);
 }
 
 function drawBall(){
+  // create the ball shape
   stroke('black');
   strokeWeight(1);
   fill('orange');
-  circle(ball.x, ball.y, 2*ball.radius);
-  line(ball.x, ball.y + ball.radius, ball.x, ball.y - ball.radius);
-  line(ball.x - ball.radius, ball.y, ball.x + ball.radius, ball.y);
-  line(ball.x - 7*ball.radius/8, ball.y + ball.radius/2, ball.x + 7*ball.radius/8, ball.y + ball.radius/2);
-  line(ball.x - 7*ball.radius/8, ball.y - ball.radius/2, ball.x + 7*ball.radius/8, ball.y - ball.radius/2);
+  circle(ball.x, ball.y, 2*ball.RADIUS);
+
+  //create the ball markings
+  line(ball.x, ball.y + ball.RADIUS, ball.x, ball.y - ball.RADIUS);
+  line(ball.x - ball.RADIUS, ball.y, ball.x + ball.RADIUS, ball.y);
+  line(ball.x - 7*ball.RADIUS/8, ball.y + ball.RADIUS/2, ball.x + 7*ball.RADIUS/8, ball.y + ball.RADIUS/2);
+  line(ball.x - 7*ball.RADIUS/8, ball.y - ball.RADIUS/2, ball.x + 7*ball.RADIUS/8, ball.y - ball.RADIUS/2);
 }
 
 function drawForegroundRim(){
+  // draw the secound layer of the rim to go in front of the ball
   stroke('red');
   strokeWeight(10);
   noFill();
-  bezier(27*width/32,postHeight+2*height/11,10,10,100,50);
+  bezier(rim.X1 - rim.RADIUS, rim.y, rim.X1 - rim.RADIUS/2, rim.y + rim.RIM_HEIGHT/2, rim.X1 + rim.RADIUS/2, rim.y + rim.RIM_HEIGHT/2, rim.X1 + rim.RADIUS, rim.y);
 }
 
 function mouseOnBall(){
-  return dist(mouseX,mouseY,ball.x,ball.y) < 3*ball.radius;
+  // return if the cursor is within the range to pick up the ball
+  return dist(mouseX,mouseY,ball.x,ball.y) < 3*ball.RADIUS;
 }
 
 function moveBall(){
@@ -161,27 +176,21 @@ function moveBall(){
 
     // Reset the verticle direction to upward
     ball.verticalDirection = 1;
+
+    // Reset the scored state
+    scored = false;
   }
-  // if (ball.y < height-height/8 && !mouseIsPressed){
-  //   ball.y += ball.yVelocity;
-  // }
-  // else{
-  //   ball.yVelocity = 0;
-  // }
   
   if (ballIn()){
     
+    //get he units per second for the x and y values
     ball.xVelocity = calculateVelocity()[0];
     ball.yVelocity = calculateVelocity()[1];
 
+    //update the speed of the ball falling in accordance to the gravity 
     if (ball.y < height-height/8 && (!mouseIsPressed || !mouseOnBall())){
       ball.yShift += 1;
     }
-    // else{
-    //   ball.yShift = 0;
-    //   ball.yVelocity = 0;
-    // }
-
 
     // Move the ball if its isnt being held
     if (!mouseIsPressed || !mouseOnBall()){
@@ -194,8 +203,12 @@ function moveBall(){
       ball.y = 7*height/8;
       ball.verticalDirection*= -1;
       ball.yShift = 0;
+      // reset the scored state
+      scored = false;
     }
-    ball.xVelocity--;
+
+    // update the speeds to create a drag and gravity system
+    ball.xVelocity -= 0.5;
     ball.y += ball.yShift/10;
   }
 }
@@ -212,7 +225,6 @@ function calculateVelocity(){
   }
 
   //return the change in X and the change in Y
-
   return [ball.changeX,ball.changeY];
 }
 
@@ -222,10 +234,17 @@ function ballIn(){
 }
 
 function detectScore(){
-  // if (ball.x)
+  // check if its in the bounds of the rim
+  if ((ball.x > rim.X1 - rim.RADIUS && ball.x < rim.X1 + rim.RADIUS) && (ball.y < rim.y + rim.RIM_HEIGHT/2 && ball.y > rim.y - rim.RIM_HEIGHT/2) && scored === false){
+    score++;
+    // update the score state to prevent rapid scoring
+    scored = true;
+  }
 }
 
 function displayScore(){
+  //Show the score in the top left
   fill('White');
-  text(`Score: ${score}`, width/4, height/8);
+  textFont('Impact', 50)
+  text(`Score: ${score}`, width/8, height/8);
 }
