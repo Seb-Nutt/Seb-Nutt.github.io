6c74a9f6111079f22420ade77e53421f6adbceba
Seb-Nutt <nuttsebastian@gmail.com>
2026-03-04 15:24:48 -0600

    ball array demo

--- 04-ball-array/sketch.js
+++ 04-ball-array/sketch.js
@@ -4,17 +4,37 @@ let ballArray = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  noStroke();
 }
 
 function draw() {
   background(220);
+  for (let ball of ballArray){
+    fill(ball.r,ball.g,ball.b);
+    ball.x += ball.dx;
+    ball.y += ball.dy;
+    
+
+    if (ball.x - ball.RADIUS >= width || ball.x + ball.RADIUS <= 0){
+      ball.x = width-ball.x;
+    }
+
+    if (ball.y - ball.RADIUS >= height || ball.y + ball.RADIUS <= 0){
+      ball.y = height-ball.y;
+    }
+    
+    circle(ball.x,ball.y,ball.RADIUS*2);
+  }
+  
+
+
 }
 
-function spawnBall() {
+function spawnBall(_x,_y) {
   let theBall = {
     RADIUS: random(10,40),
-    x: random(width),
-    y: random(height),
+    x: _x,
+    y: _y,
     r: random(255),
     g: random(255),
     b: random(255),
@@ -24,16 +44,10 @@ function spawnBall() {
 
   ballArray.push(theBall);
 
-  for (let ball of ballArray){
-    fill(ball.r,ball.g,ball.b);
-    ball.x += ball.dx;
-    ball.y += ball.dy;
 
-    circle(ball.x,ball.y,ball.RADIUS*2);
-  }
+
 
 }
-
 function mousePressed(){
-  spawnBall();
+  spawnBall(mouseX,mouseY);
 }
