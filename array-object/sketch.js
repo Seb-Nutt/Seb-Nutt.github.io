302391a803cfd96b5ff4ffa959af894f575abac3
Seb-Nutt <nuttsebastian@gmail.com>
2026-03-05 15:18:58 -0600

    Created the array object assignment

---
+++ array-object/sketch.js
@@ -0,0 +1,15 @@
+// Arrays and object notation
+// Sebastian Nutt
+// March 5th 2026
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
