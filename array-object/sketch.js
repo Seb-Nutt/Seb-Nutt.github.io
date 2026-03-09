cac5fd9fb9813cd2a72dc118157ab9350dae24d3
Seb-Nutt <nuttsebastian@gmail.com>
2026-03-09 15:25:35 -0600

    created a system to listen to the connected mic when the left alt key is held

--- array-object/sketch.js
+++ array-object/sketch.js
@@ -5,14 +5,44 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-// 3d envirement that allows you to speak with an ai through the p5.speech module speech to text and responding via text to speech
+// 3d envirement that allows you to speak to a thing that will repeat what yu said back to it via text to speech
+
+let playerSpeech;
+let speech = new p5.Speech();
+let speechRec;
+let listening;
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  speechRec = new p5.SpeechRec('en-US', gotSpeech);
+  speechRec.start();
+
+  function gotSpeech(){
+    console.log(speechRec);
+  }
+
 }
 
 function draw() {
   background(220);
-  
+  listen();
+  speak();
+  console.log(speechRec.resultString);
+}
+
+function speak(){
+
+}
+
+function listen(){
+  //listen if the alt key is held down
+  if (!keyIsDown(18)){
+    speechRec.stop();
+    listening = false;
+  }
+  else if (!listening){
+    speechRec.start();
+    listening = true;
+  }
 }
