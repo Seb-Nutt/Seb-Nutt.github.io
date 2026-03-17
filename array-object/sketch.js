be1cb1dcb19d2db45ee3b550850e058f9ae8ea08
Seb-Nutt <nuttsebastian@gmail.com>
2026-03-17 15:27:42 -0600

    Added Noise demo, finalised array object speech processing and added comments

--- array-object/sketch.js
+++ array-object/sketch.js
@@ -5,46 +5,57 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-// 3d envirement that allows you to speak to a thing that will repeat what yu said back to it via text to speech
 
+//create the object for replying via text to speech
 let speech = new p5.Speech();
+
+//create the response model
+let rm = RiTa.markov(2,{trace: false}, {disableInputChecks: true});
+
 let speechRec;
 let listening;
+// display the prompt to speak initially before chagning to display the users input
 let humanSpeech = 'Hold V to speak';
-let rm = RiTa.markov(2,{trace: false}, {disableInputChecks: true});
-// rm.untokenize;
-let textAdded = false;
-let processed = true;
 let generated;
 let hamlet;
-let trainingWords = ['is','the','or'];
-let result;
+let textBox = {
+  x: 0,
+  y: 0,
+  boxWidth: 0,
+  BOXHEIGHT: 50
+};
 
 function preload(){
-  // attempting to load hamlet to give the model something to train off of
+  // load hamlet to give the model something to train off of
   hamlet = loadStrings('/hamlet.txt');
   
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  speechRec = new p5.SpeechRec('en-US'); //gotSpeech);
 
-  while (trainingWords.length < 1000){
-    trainingWords.push(RiTa.randomWord());
-  }
+  // create the speech recognition object
+  speechRec = new p5.SpeechRec('en-US');
 
+  //align text to the center
+  textAlign('center');
+
+  //add hamlet into the AI's processing
   rm.addText(hamlet);
-  // rm.addText(trainingWords);
 
+  textBox.y = height/2-25;
+
+  //create a callback to run the speech recognition when the speech recognition gets a result
   speechRec.onResult = processSpeech;
 }
 
 function draw() {
   background(220);
 
+  //constantly listen and dsplay text
   listen();
   displayText();
+  drawTextBox();
 }
 
 function speak(){
@@ -61,31 +72,45 @@ function listen(){
   else if (!listening && keyIsDown(86)){
     speechRec.start();
     listening = true;
-    processed = false;
   }
 }
 
 function processSpeech(){
+  // get the result of the speech synthesis
   humanSpeech = speechRec.resultString;
-  // humanSpeech = humanSpeech.split(' ');
 
+  // add the users input into the model
   rm.addText(humanSpeech);
+
+  // create the output in the form a list which predicts the next word based on the latest added word
   generated = [humanSpeech];
-  for (let i = 0; i < 9; i++){
-    generated.push(rm.completions(generated[i])[0]);
+  while (generated[generated.length-1] !== '.' || generated.length === 1){
+    generated.push(random(rm.completions(generated[generated.length-1])));
   }
 
-  processed = true;
-  
-  console.log(generated);
+  // remove the initial human input to properly format the response
+  generated.shift();
 
+  //rejoin the output into a string
+  generated = generated.join(' ');
+
+  // call the text to speech to speak the output
   speak();
 }
 
 function displayText(){
+  //display both the users input and the Models output
+  text(humanSpeech,width/2,height/6);
+  text(generated, width/2,height/2 + 50);
+}
 
-  if (humanSpeech !== 'Hold V to Speak'){
-    text(humanSpeech,width/2,height/2);
-    text(generated, width/2,height/2 + 50);
+function drawTextBox(){
+  fill(100);
+  try{
+    textBox.boxWidth = generated.length*10;
+    textBox.x = width/2 - generated.length;
+  }
+  catch{
+    square(textBox.x,height/2-50,textBox.boxWidth,textBox.BOXHEIGHT);
   }
 }
