0061ca97f83f18050885409a9415f197841569de
Seb-Nutt <nuttsebastian@gmail.com>
2026-03-20 15:25:42 -0600

    neighbors demo

---
+++ 08-rect-grid/sketch.js
@@ -0,0 +1,89 @@
+// 2d rectangular grid demo
+
+const CELL_SIZE = 100;
+let rows;
+let cols;
+let grid;
+let white = false;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  rows = Math.floor(height/CELL_SIZE);
+  cols = Math.floor(width/CELL_SIZE);
+  grid = generateGrid(cols,rows);
+}
+
+function draw() {
+  background(220);
+
+
+  displayGrid();
+}
+
+function generateGrid(cols,rows){
+  newGrid = [];
+  for (let y = 0; y < cols; y++){
+    newGrid.push([]);
+    for (let x = 0; x < rows; x++){
+      clr = random(100);
+      if (clr < 50 || white){
+        newGrid[y].push(0);
+      }
+      else{
+        newGrid[y].push(1);
+      }
+    }
+  }
+  return newGrid;
+}
+
+
+
+function displayGrid(){
+  for (let y = 0; y < cols; y++){
+    for (let x = 0; x < rows; x++){
+      if (grid[y][x] === 0){
+        fill('white');
+      }
+      else if(grid[y][x] === 1){
+        fill('black');
+      }
+
+      square(x*CELL_SIZE,y*CELL_SIZE,CELL_SIZE);
+    }
+  }
+}
+
+function mousePressed(){
+  let col = Math.floor(mouseY/CELL_SIZE);
+  let row = Math.floor(mouseX/CELL_SIZE);
+
+  toggleCell(col,row);
+  toggleCell(col+1,row);
+  toggleCell(col-1,row);
+  toggleCell(col,row+1);
+  toggleCell(col,row-1);
+}
+
+function toggleCell(col,row){
+
+  if (col >= 0 && col < cols && row >= 0 && row < rows){
+    if (grid[col][row] === 1){
+      grid[col][row] = 0;
+    }
+    else if (grid[col][row] === 0){
+      grid[col][row] = 1;
+    }
+  }
+}
+
+function keyPressed() {
+  if (key === 'r'){
+    white = false;
+    grid = generateGrid(cols,rows);
+  }
+  else if (key === 'e'){
+    white = true;
+    grid = generateGrid(cols,rows);
+  }
+}
